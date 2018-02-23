90e63b62fc60634261c481dc653b2770c4d84de1
LVilchez <lau.16.9@gmail.com>
2018-02-23 13:13:36 +0100

    Codigo basico de peon del personaje. Control de cámara + input configurado.

--- Source/ProyectoAnubis/Prota.cpp
+++ Source/ProyectoAnubis/Prota.cpp
@@ -1,34 +1,74 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Prota.h"
+#include "Runtime/Engine/Classes/Components/CapsuleComponent.h"
+#include "Runtime/Engine/Classes/Components/SkeletalMeshComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
-
-// Sets default values
 AProta::AProta()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Creo la capsula de colision del personaje
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsula"));
+	CapsuleComponent->InitCapsuleSize(34.0f, 88.0f);
+	CapsuleComponent->SetCollisionProfileName("Felipe");
+	CapsuleComponent->CanCharacterStepUpOn = ECB_No;
+	CapsuleComponent->bShouldUpdatePhysicsVolume = true;
+	CapsuleComponent->bCheckAsyncSceneOnMove = false;
+	CapsuleComponent->SetCanEverAffectNavigation(false);
+	CapsuleComponent->bDynamicObstacle = true;
+	RootComponent = CapsuleComponent;
+
+	// Agrero el spring arm para la camara
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.0f;
+
+	// EL BRAZO ROTA SEGUN EL INPUT QUE LE LLEGA AL PEON
+	CameraBoom->bUsePawnControlRotation = true; 
+
+	// Agrego la camara
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+
+	// LA CAMARA EN SI NO ROTA SEGUN EL INPUT QUE LE LLEGA AL PEON
+	FollowCamera->bUsePawnControlRotation = false;
+
+	Mesh = CreateOptionalDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	if (Mesh)
+	{
+		Mesh->AlwaysLoadOnClient = true;
+		Mesh->AlwaysLoadOnServer = true;
+		Mesh->bOwnerNoSee = false;
+		Mesh->MeshComponentUpdateFlag = EMeshComponentUpdateFlag::AlwaysTickPose;
+		Mesh->bCastDynamicShadow = true;
+		Mesh->bAffectDynamicIndirectLighting = true;
+		Mesh->PrimaryComponentTick.TickGroup = TG_PrePhysics;
+		Mesh->SetupAttachment(CapsuleComponent);
+		static FName MeshCollisionProfileName(TEXT("CharacterMesh"));
+		Mesh->SetCollisionProfileName(MeshCollisionProfileName);
+		Mesh->bGenerateOverlapEvents = false;
+		Mesh->SetCanEverAffectNavigation(false);
+	}
 }
 
-// Called when the game starts or when spawned
 void AProta::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void AProta::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void AProta::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 }
 
--- Source/ProyectoAnubis/Prota.h
+++ Source/ProyectoAnubis/Prota.h
@@ -11,6 +11,18 @@ class PROYECTOANUBIS_API AProta : public APawn
 {
 	GENERATED_BODY()
 
+	/** The CapsuleComponent being used for movement collision (by CharacterMovement). Always treated as being vertically aligned in simple collision check functions. */
+	UPROPERTY(Category = Character, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class UCapsuleComponent* CapsuleComponent;
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* FollowCamera;
+
 public:
 	// Sets default values for this pawn's properties
 	AProta();
@@ -26,6 +38,10 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	/** The main skeletal mesh associated with this Character (optional sub-object). */
+	UPROPERTY(Category = Character, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+		class USkeletalMeshComponent* Mesh;
 	
 	
 };
